fe7c447c8dd27d05041a88057cfdc7b05fbffc87
Ahmed Saladin <as.yuri74@gmail.com>
2019-08-17 15:49:42 +0200

    Handlebar Engine

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
@@ -9,7 +10,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
