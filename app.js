e8a132ee39f9dcec5ec2329d638fc41afc4efcad
Ahmed Saladin <as.yuri74@gmail.com>
2019-08-17 18:10:41 +0200

    EJS Template Engine

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+//set template engine
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// routes
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  routes
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+//middleware handle unknown routes
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
-app.listen(3000);
+// port setup
+app.listen(3000, () => {
+  console.log('Server is running');
+});
