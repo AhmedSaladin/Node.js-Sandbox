3f9a831b2788b442bbfcb45b3ced53ddb1068fa7
Ahmed Saladin <as.yuri74@gmail.com>
2019-09-02 01:00:44 +0200

    styling login form

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 // routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 
 // middlewares
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 //  routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 //middleware handle unknown routes
 app.use(errorController.get404);
