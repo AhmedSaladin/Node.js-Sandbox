71aba68d599fb3c72793694329bd905ecb769d93
Ahmed Saladin <as.yuri74@gmail.com>
2019-09-22 06:26:34 +0200

    adding dynamic pdf to order

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const PDFDocument = require('pdfkit');
 //
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -150,13 +151,32 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
-      const file = fs.createReadStream(invoicePath);
+      const pdfDoc = new PDFDocument();
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
         'inline; filename="' + invoiceName + '"'
       );
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.fontSize(26).text('Invoice');
+      pdfDoc.text('------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              '                     Quantity: ' +
+              prod.quantity +
+              '                     Price By Pice: ' +
+              prod.product.price
+          );
+      });
+      pdfDoc.text('------------------');
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+      pdfDoc.end();
     })
     .catch(err => next(err));
 };
