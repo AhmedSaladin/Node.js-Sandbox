aef2549f4162ab082b77ab9ab4f4d5c728855532
Ahmed Saladin <as.yuri74@gmail.com>
2019-09-23 17:49:42 +0200

    simple app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const feedRoutes = require('./routes/feed');
+
+// app.use(bodyParser.urlencoded({}));
+
+app.use(express.json());
+
+//disable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // star means it enable for all domains
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Header', '*');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen('8080', () => console.log('Server is running on port 8080'));
